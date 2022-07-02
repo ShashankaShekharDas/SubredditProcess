b74b50b5a55f809786b59433e4aa44a5d89037e0
Shashanka <gudurajasekarreddy@gmail.com>
2022-07-02 21:01:37 +0530

    Added column name to csv

--- plugins/read_from_db.sh
+++ plugins/read_from_db.sh
@@ -1,3 +1,5 @@
 gsutil rm gs://reddit-info/Subreddit/subreddit.csv
 gcloud sql export csv reddit-managed-storage gs://reddit-info/Subreddit/subreddit.csv --query='SELECT * FROM reddit.subreddits;'
 gsutil cp -A gs://reddit-info/Subreddit/subreddit.csv /home/airflow_exec/airflow-medium/data/subreddit.csv
+#Added column name to the file for easier accessing
+echo 'subreddit_name,start_post\n' | cat - /home/airflow_exec/airflow-medium/data/subreddit.csv > temp && mv temp /home/airflow_exec/airflow-medium/data/subreddit.csv
